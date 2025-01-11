f722812eacc14d07473da6df94e2ae3a8c65d4f6
Kourosh <picorog83@gmail.com>
2025-01-11 06:48:36 +0330

    add test program for new scheduler

---
+++ user/testSchd.c
@@ -0,0 +1,32 @@
+#include "user.h"
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+
+int main() {
+    int highUse = 0;
+    int pid = fork();
+    if(pid == 0){
+        pid = fork();
+        if(pid == 0){
+            highUse = 1;
+            for(int i = 0;i < 3;i++){
+                fork();
+            }
+            for(int i = 0;i <= 200000000;i++);
+            sleep(10);
+        }else{
+            sleep(10);
+            for(int i = 0 ;i < 3;i++) {
+                fork();
+            }
+            sleep(2);
+        }
+    }
+    sleep(2);
+    if(highUse){
+        printf("1\n");
+    }else{
+        printf("2\n");
+    }
+    while (1);
+}
