0a24e2e149cc66c37778a5ff1582ff0eba1defaa
Kourosh <picorog83@gmail.com>
2025-01-13 15:41:28 +0330

    changed testThrd.c to be the test program of set_quota

--- user/testThrd.c
+++ user/testThrd.c
@@ -2,33 +2,34 @@
 #include "../kernel/types.h"
 #include "../kernel/stat.h"
 
-volatile int a = 0, b = 0, c = 0;
-
-void *my_thread(void *arg);
-
 int main() {
-    int ta = create_thread(my_thread, (void *) &a);
-    int tb = create_thread(my_thread, (void *) &b);
-    int tc = create_thread(my_thread, (void *) &c);
-    printf("CJ\n");
-    join_thread(ta);
-    join_thread(tb);
-    join_thread(tc);
-    printf("ta: %d\ttb: %d\ttc: %d\n",ta,tb,tc);
-    exit(0);
-}
-
-void *my_thread(void *arg) {
-    int *number = (int *) arg;
-    for (int i = 0;i < 50; ++i) {
-        *number = *number + 1;
-        if (number == &a) {
-            printf("a: %d\n", *number);
-        } else if (number == &b) {
-            printf("b: %d\n", *number);
-        } else {
-            printf("c: %d\n", *number);
+    int highUse = 0;
+    int pid = fork();
+    if(pid == 0){
+        pid = fork();
+        if(pid == 0){
+            highUse = 1;
+            for(int i = 0;i < 3;i++){
+                fork();
+            }
+            for(int i = 0;i <= 200000000;i++);
+            set_cpu_quota(getpid(),1);
+            sleep(2);
+        }else{
+            for(int i = 0;i < 3;i++){
+                fork();
+            }
+            for(int i = 0;i <= 200000000;i++);
+            sleep(2);
         }
+    }else{
+        while (1);
     }
-    return (void *) number;
+    sleep(2);
+    if(highUse){
+        printf("1\n");
+    }else{
+        printf("2\n");
+    }
+    while (1);
 }
